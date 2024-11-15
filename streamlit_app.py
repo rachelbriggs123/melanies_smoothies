833ac89c7a36a3f6909952104a49f00b6e49eb38
rachelbriggs123 <rachel.briggs@integritypro.com>
2024-11-15 08:41:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response
+st.text(smoothiefroot_response)
 
 
 
