14b3d9679182c286c793b7da01063c2e6e49e9e7
rachelbriggs123 <rachel.briggs@integritypro.com>
2024-11-15 09:03:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,8 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
+session.close()
+
 
 
 
